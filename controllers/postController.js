dd9c817c6298365d0be3c03a0bcbb9feaeb495b7
giorgio-marzo <gio.marzo96@gmail.com>
2025-04-22 16:43:05 +0200

    [UPD] funzzione destroy e esportazione

--- controllers/postController.js
+++ controllers/postController.js
@@ -24,3 +24,10 @@ function update(req, res) {
 function modify(req, res) {
     res.send(`modifica parziale post con id: ${req.params.id} `);
 }
+
+//destroy
+function destroy(req, res) {
+    res.send(`rimozione totale post con id: ${req.params.id} `);
+}
+
+module.exports = { index, show, store, update, modify, destroy }
