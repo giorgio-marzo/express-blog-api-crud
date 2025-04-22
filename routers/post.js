223d7f4e2b77895de088bfa9511a7803e4e2c0f7
giorgio-marzo <gio.marzo96@gmail.com>
2025-04-22 17:08:38 +0200

    [UPD] importo controller nel router e Milestone 1 completata

--- routers/post.js
+++ routers/post.js
@@ -5,36 +5,27 @@ const express = require('express');
 
 const router = express.Router();
 
+// importazione controller
+const postController = require(`../controllers/postController.js`)
+
 // difiniamo le rotte delle operazioni di post attraverso i metodi get, post, put ec...
 
 // rotta index
-router.get('/', (req, res) => {
-    res.send('indice dei post')
-})
+router.get('/', postController.index);
 
 // operazione show
-router.get('/:id', (req, res) => {
-    res.send(`post con id: ${req.params.id} `)
-})
+router.get('/:id', postController.show);
 
 // store
-router.post('/', (req, res) => {
-    res.send(`aggiunta nuovo post`)
-})
+router.post('/', postController.store);
 
 //update
-router.put('/:id', (req, res) => {
-    res.send(`modifica totale post con id: ${req.params.id} `)
-})
+router.put('/:id', postController.update);
 
 //modify
-router.patch('/:id', (req, res) => {
-    res.send(`modifica parziale post con id: ${req.params.id} `)
-})
+router.patch('/:id', postController.modify);
 
 //destroy
-router.delete('/:id', (req, res) => {
-    res.send(`rimozione totale post con id: ${req.params.id} `)
-})
+router.delete('/:id', postController.destroy);
 
 module.exports = router;
