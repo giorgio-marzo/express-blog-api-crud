baca61f709715b23fedc867bc9b43a6b132ad532
giorgio-marzo <gio.marzo96@gmail.com>
2025-04-22 16:31:50 +0200

    [UPD] funzioni show e store

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,3 +5,12 @@ function index(req, res) {
     res.send('indice dei post');
 }
 
+//show
+function show(req, res) {
+    res.send(`post con id: ${req.params.id} `);
+}
+
+//store
+function store(req, res) {
+    res.send(`aggiunta nuovo post`);
+}
